4efcc5d3dc0388e14df982dd823a2e543e3ea566
3lCattivo <49725816+3lCattivo@users.noreply.github.com>
2022-03-01 20:32:19 +0100

    only exec php if form is sent

--- index.php
+++ index.php
@@ -53,20 +53,21 @@
 		<button type="submit">Hinzufügen!</button> 
 
 		<?php
+			if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 
-			function convertToWindowsCharset($string) {
-				$charset =  mb_detect_encoding($string,"UTF-8, ISO-8859-1, ISO-8859-15",true);
-				$string =  mb_convert_encoding($string, "Windows-1252", $charset);
-				return $string;
+				function convertToWindowsCharset($string) {
+					$charset =  mb_detect_encoding($string,"UTF-8, ISO-8859-1, ISO-8859-15",true);
+					$string =  mb_convert_encoding($string, "Windows-1252", $charset);
+					return $string;
+				}
+				$myfile = fopen("stricherliste.csv", "r") or die("Unable to open file!");
+				$list = fgetcsv($myfile);
+				fclose($myfile);
+				$myfile = fopen("stricherliste.csv", "a") or die("Unable to open file!");
+				$txt = strval($_POST["name"]) . ";Bier;" . strval($_POST["bier"]) . ";Toast;" . strval($_POST["toast"]) . "\n";
+				fwrite($myfile, convertToWindowsCharset($txt));
+				fclose($myfile);
 			}
-
-			$myfile = fopen("stricherliste.csv", "r") or die("Unable to open file!");
-			$list = fgetcsv($myfile);
-			fclose($myfile);
-			$myfile = fopen("stricherliste.csv", "a") or die("Unable to open file!");
-			$txt = strval($_POST["name"]) . ";Bier;" . strval($_POST["bier"]) . ";Toast;" . strval($_POST["toast"]) . "\n";
-			fwrite($myfile, convertToWindowsCharset($txt));
-			fclose($myfile);
 		 
 	
 		?>
